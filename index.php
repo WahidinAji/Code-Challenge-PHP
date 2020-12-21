73d8e585f4c90724aa4edf913a5fecaefa6808e2
WahidinAji <a17wahidin@gmail.com>
2020-12-22 00:03:40 +0700

    sorting by karakter per index

--- index.php
+++ index.php
@@ -1,13 +1,53 @@
 <?php
-
-function StringChallenge($str)
+function FunctionName($longestKey)
 {
-  echo "Input: ";
-  $words = fopen("php://stdin", "r");
-  $input = trim(fgets($words));
-  $ex = explode(" ", $input);
-  $im = implode($ex);
-  $str = $im == strrev($im) ? 'true' : 'false';
-  return $str;
+  $array = array("coder", "byte", "code");
+  // $array = array("abc", "defg", "z", "hijk");
+  usort($array, function ($a, $b) {
+    return strlen($a) > strlen($b);
+  });
+  // $p = print_r($array);
+  print_r("Input: ") . print_r($array);
+  array_pop($array);
+  array_pop($array);
+  $longestKey = array_reduce($array, function ($a, $b) {
+    $var =  strlen($a) > strlen($b) ? $a : $b;
+    return $var;
+  });
+
+  return "Output: " . $longestKey;
 }
-echo StringChallenge(fgetc(fopen('E:\laragon\www\TestJavan\index.php', 'r')));
+echo FunctionName(fgets(fopen('E:\laragon\www\TestJavan\index.php', 'r')));
+
+// function ArrayChallenge($strArr)
+// {
+//   $var = array("coder", "byte", "code");
+//   $c = $var[0];
+//   $len = strlen($c);
+//   return $len;
+// }
+// echo ArrayChallenge(fgets(fopen('E:\laragon\www\TestJavan\index.php', 'r')));
+
+
+
+// function FunctionName($longestKey)
+// {
+//   $array = array('ss', 'ggg', 'ddfdfddf', 'ssssdssssss', 'saadadsadasdasdasasd');
+//   array_pop($array);
+//   $longestKey = array_reduce($array, function ($a, $b) {
+//     $var =  strlen($a) > strlen($b) ? $a : $b;
+//     return $var;
+//   });
+//   return $longestKey;
+// }
+// echo FunctionName(fgets(fopen('E:\laragon\www\TestJavan\index.php', 'r')));
+  
+  
+  
+  //  $colors = array("red", "green", "blue", "yellow");
+  // foreach ($colors as $value) {
+  //   // strlen($value);
+  //   // $min = min($value);
+  //   // min($value);
+  //   echo $value . "\n";
+  // }
